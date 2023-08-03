f4a276939538cf5dba60e80564e413e09ad158a0
thatejguy <ejgrotto@gmail.com>
2023-08-03 20:00:12 +1000

    grease

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const container = document.querySelector('.container');
 
 //stuff
 function makeGrid(rows, cols) {
-  container.style.setProperty('grease', rows);
-  container.style.setProperty('grease', cols);
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < rows * cols; i++) {
     let div = document.createElement('div');
-    div.innerText = (i + 1);
     container.appendChild(div).className = 'grid-item';
   }
 }
 
 makeGrid(16, 16);
+
+
+
