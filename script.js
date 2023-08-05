3e31f8c9fa6044199c17f0214280fe55e34fdf79
thatejguy <ejgrotto@gmail.com>
2023-08-06 00:47:36 +1000

    getting there. with overflow hidden property

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function createDivs(num) {
   for (let i = 0; i < num * num; i++) {
     let grid = document.createElement('div');
     grid.classList.add('grid');
+    grid.style.width = `${500 / num}px`;
+    grid.style.height = `${500 / num}px`;
     container.appendChild(grid);
   }
 }
 
-createDivs(16);
+createDivs(56);
 
 
 
